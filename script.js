c002c41bab1e882fcb5a8135de3ab9e2b4ea83c1
Luiz Eduardo <luizleitefe@gmail.com>
2023-03-22 16:24:48 -0300

    alguma aula ai
    legal mais nao consegui tudo kkkkk

---
+++ script.js
@@ -0,0 +1,16 @@
+// Verifique se a sua idade é maior do que a de algum parente
+
+
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 15
+var idadeParente = 14
+
+if (minhaIdade > idadeParente){
+  console.log('maior')
+}
+else if(minhaIdade === idadeParente){
+  console.log('igual')
+}
+else {
+  console.log('menor')
+}
