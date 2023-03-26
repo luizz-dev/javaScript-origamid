d7d7ee3cb316709511b2de69563ef1ee53b11b10
Luiz Eduardo <luizleitefe@gmail.com>
2023-03-26 16:37:17 -0300

    Update script.js
    funcaoes 1

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Verifique se a sua idade é maior do que a de algum parente
-
-
-// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
-var minhaIdade = 15
-var idadeParente = 14
-
-if (minhaIdade > idadeParente){
-  console.log('maior')
-}
-else if(minhaIdade === idadeParente){
-  console.log('igual')
-}
-else {
-  console.log('menor')
+function corFavorita(cor){
+  if(cor === 'verde'){
+    return 'verde é a cor do dinheiro'
+  }
+  else if (cor === 'rosa'){
+    return 'rosa é tambem uma flor'
+  }
+  else {
+  return 'nao gosto de cor'
+  }
 }
