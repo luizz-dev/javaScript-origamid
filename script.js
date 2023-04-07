56cee703b2b5c77ff40fe5a53f0a8e233fe1dbf2
Luiz Eduardo <luizleitefe@gmail.com>
2023-04-07 15:35:25 -0300

    sadad
    asdaads

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-function corFavorita(cor){
-  if(cor === 'verde'){
-    return 'verde é a cor do dinheiro'
+function Corfavorita(cor){
+  if (cor === 'verde'){
+    return "verde é a cor do dinheiro"
   }
-  else if (cor === 'rosa'){
-    return 'rosa é tambem uma flor'
-  }
-  else {
+else if (cor ==='amarelo'){
+  return 'amarelo é a cor do sol'
+}
+
+else {
   return 'nao gosto de cor'
-  }
 }
-
-function terceiraIdade(idade){
-  if(typeof idade !== 'number'){
-    return 'por favor coloque um numero'
-  }
-  else if (idade >= 60){
-  return true
-  }
-  else {
-  return false
-  }
-
 }
-console.log(terceiraIdade(60))
+Corfavorita()
+
+
 
