3d99de66079bbc86e7c89e8fddfa47a02bbcb079
Luiz Eduardo <luizleitefe@gmail.com>
2023-04-20 18:34:02 -0300

    legal consegui 2
    aula objetos acabada

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function Corfavorita(cor){
-  if (cor === 'verde'){
-    return "verde é a cor do dinheiro"
+var dados = {
+  nome:'luiz',
+  sobrenome:'eduardo',
+  idade:14,
+  sexo:'masculino'
+}
+
+dados.nomeCompleto = function(){
+  return `${this.nome} ${this.sobrenome}`
+}
+
+var carro = {
+  preco:1000,
+  portas:4,
+  marca:'audi',
+}
+
+carro.preco=3000
+
+
+var cachorro ={
+  raca:'labrador',
+  cor:'preto',
+  idade:10,
+  latir(pessoas){
+    if(pessoas==='homem')
+    return 'latir'
+    else {
+      return'nada'
+    }
   }
-else if (cor ==='amarelo'){
-  return 'amarelo é a cor do sol'
 }
-
-else {
-  return 'nao gosto de cor'
-}
-}
-Corfavorita()
-
-
-
