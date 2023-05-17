44751559ed71dccc0a35cd9ae2702c600ca90e61
Luiz Eduardo <luizleitefe@gmail.com>
2023-05-17 17:36:57 -0300

    aula final de array  e loop
    consegui

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var brasilGanhou=['1959','1962','1970','1994','2002']
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilGanhou = ['1959','1962','1970','2002',]
 
-for(var i = 0 ;i < brasilGanhou.length ;i++){
-  console.log(`O Brasil ganhou de ${brasilGanhou [i]}`)
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for(var ganhou=0;ganhou<brasilGanhou.length;ganhou++){
+  console.log(`O brasil ganhou em ${brasilGanhou[ganhou]}`)
 }
+
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+ for (var fruta=0;fruta<frutas.length;fruta++){
+  console.log(frutas[fruta])
+  if(frutas[fruta]==='Pera')
+  break
+ }
+
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length -1]
