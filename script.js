a5fd0bc410588b26d34569352a504a8292ec5d73
Luiz Eduardo <luizleitefe@gmail.com>
2023-05-29 16:22:51 -0300

    hjg

--- script.js
+++ script.js
@@ -1,56 +1,10 @@
-// Some 500 ao valor de scroll abaixo,
-// atribuindo o novo valor a scroll
-var scroll = 1000;
-scroll += 500;
-console.log(scroll);
+const botao = document.querySelector("button");
 
-// Atribua true para a variável darCredito,
-// caso o cliente possua carro e casa.
-// E false caso o contrário.
-var possuiCarro = true;
-var possuiCasa = true;
-var darCredito;
+function (){
 
-darCredito = possuiCarro && possuiCasa;
 
-console.log(darCredito);
-
-const data = {
-  ano: 2018,
-  mes: "dezembro",
-};
-
-console.log(data);
-
-// Por qual motivo o código abaixo retorna com erros?
-{
-  var cor = "preto";
-  const marca = "Fiat";
-  let portas = 4;
-  console.log(cor, marca, portas);
+if (botao === "nao aceito") {
+  window.alert("vai namorar sim");
+  console.log(botao);
 }
-
-// vaza por causa que var pode ser redefinido
-
-// Como corrigir o erro abaixo?
-const dois = 2;
-function somarDois(x) {
-  return x + dois;
 }
-function dividirDois(x) {
-  return x / dois;
-}
-somarDois(4);
-dividirDois(6);
-
-//porque a const nao da escop
-
-// O que fazer para total retornar 500?
-const numero = 50;
-
-for (let numero = 0; numero < 10; numero++) {
-  console.log(numero);
-}
-
-const total = 10 * numero;
-console.log(total);
