040864e6782de70fb042ded1e7005cc9ef8b983e
Luiz Eduardo <luizleitefe@gmail.com>
2023-05-29 19:00:12 -0300

    consegui de 2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const botao = document.querySelector("button");
-
-function (){
-
-
-if (botao === "nao aceito") {
-  window.alert("vai namorar sim");
-  console.log(botao);
-}
-}
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elemento = document.querySelector(".bla");
+console.log(elemento);
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
+console.log(window.innerWidth);
