0a0321e9903f73a8cc8993d6bb9f66b47819c276
Luiz Eduardo <luizleitefe@gmail.com>
2023-06-06 19:05:13 -0300

    dsada
    dsad

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// Retorne o url da página atual utilizando o objeto window
-const url = window.location.href;
-console.log(url);
-// Seleciona o primeiro elemento da página que
-// possua a classe ativo
-const elemento = document.querySelector(".bla");
-console.log(elemento);
-// Retorne a linguagem do navegador
-const linguagem = window.navigator.language;
-console.log(linguagem);
-// Retorne a largura da janela
-console.log(window.innerWidth);
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSections = document.getElementsByClassName("grid-sections");
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
