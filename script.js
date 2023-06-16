2c6ef61c2966dedb0294bd33be50d49ff512504d
Luiz Eduardo <luizleitefe@gmail.com>
2023-06-16 17:49:29 -0300

    entendi e consegui

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ console.log(img);
 const imagem = document.querySelectorAll('img[src^="img/imagem"]');
 console.log(imagem);
 // Selecione todos os links internos (onde o href começa com #)
-// const linksInternos = document.getElementById("#");
-// console.log(linksInternos);
+const linksInternos = document.querySelectorAll("[href^='#']");
+console.log(linksInternos);
 // Selecione o primeiro h2 dentro de .animais-descricao
-
+const primeiroH2 = document.querySelector(".animais-descricao h2");
+console.log(primeiroH2);
 // Selecione o último p do site
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos[paragrafos.length - 1]);
