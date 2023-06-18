2959ee2d7f875161b4766b5d1e5216aa236d97cc
Luiz Eduardo <luizleitefe@gmail.com>
2023-06-18 18:21:34 -0300

    consegui
    legal

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// Retorne no console todas as imagens do site
+// Mostre no console cada parágrado do site
+const p = document.querySelectorAll("p");
+console.log(p);
 
-const img = document.querySelectorAll("img");
-console.log(img);
+p.forEach((item) => {
+  console.log(item);
+});
+// Mostre o texto dos parágrafos no console
+p.forEach((item) => {
+  console.log(item.in);
+});
 
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-const imagem = document.querySelectorAll('img[src^="img/imagem"]');
-console.log(imagem);
-// Selecione todos os links internos (onde o href começa com #)
-const linksInternos = document.querySelectorAll("[href^='#']");
-console.log(linksInternos);
-// Selecione o primeiro h2 dentro de .animais-descricao
-const primeiroH2 = document.querySelector(".animais-descricao h2");
-console.log(primeiroH2);
-// Selecione o último p do site
-const paragrafos = document.querySelectorAll("p");
-console.log(paragrafos[paragrafos.length - 1]);
+// Como corrigir os erros abaixo:
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
