f8a3a8ac21724223de4d5c00a22b3cbc9e07aa0d
Luiz Eduardo <luizleitefe@gmail.com>
2023-06-26 15:41:54 -0300

    nao

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-// Mostre no console cada parágrado do site
-const p = document.querySelectorAll("p");
-console.log(p);
+// Adicione a classe ativo a todos os itens do menu
+const classeA = document.querySelectorAll(".menu");
+classeA.classList.add("ativo");
 
-p.forEach((item) => {
-  console.log(item);
-});
-// Mostre o texto dos parágrafos no console
-p.forEach((item) => {
-  console.log(item.in);
-});
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
-// Como corrigir os erros abaixo:
+// Verifique se as imagens possuem o atributo alt
+const imagens = document.querySelectorAll("img");
 
-const imgs = document.querySelectorAll("img");
-
-imgs.forEach((item, index) => {
-  console.log(item, index);
-});
-
-let i = 0;
-imgs.forEach(() => {
-  console.log(i++);
-});
-
-imgs.forEach(() => i++);
+// Modifique o href do link externo no menu
