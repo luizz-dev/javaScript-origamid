4c7105a7b082992afd8860fa005fd819aceb1cb3
Luiz Eduardo <luizleitefe@gmail.com>
2023-07-05 17:59:30 -0300

    f
    f

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
-const dImg = document.querySelector("img");
-console.log(dImg.offsetTop);
+const img = document.querySelector("img");
+const imgTop = img.offsetTop;
+
+console.log(imgTop);
+
 // Retorne a soma da largura de todas as imagens
-const TImg = document.querySelectorAll("img");
-console.log(TImg.offsetLeft);
+function somaImagens() {
+const imgs = document.querySelectorAll("img");
+let soma=0
+imgs.forEach((imagens) => {
+  soma = soma + img.offsetWidth
+  console.log(soma);
+});
+
+window.onload = function () {
+somaImagens()
+ }
+
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
-
-if (window.innerHeight < 720) {
-  classlist.add("menu-mobile");
-}
