ce5786e97fee9b3b79ad4c26994d0af0ab9d2815
Luiz Eduardo <luizleitefe@gmail.com>
2023-09-05 21:01:56 -0300

    sad
    asd

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function handlekink() {
+  linksInternos.classList.add("ativo");
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener("clik", handlekink);
+});
+console.log(linksInternos);
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
