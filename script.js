ec148bc48b8d79a711e295eaa64fd9499a8f84fa
Luiz Eduardo <luizleitefe@gmail.com>
2023-09-09 16:14:29 -0300

    lgl

--- script.js
+++ script.js
@@ -4,18 +4,40 @@
 // o comportamento padrão desses links
 const linksInternos = document.querySelectorAll('a[href^="#"]');
 
-function handlekink() {
-  linksInternos.classList.add("ativo");
+function handlekink(event) {
+  event.preventDefault();
+  linksInternos.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
 }
 
 linksInternos.forEach((link) => {
-  link.addEventListener("clik", handlekink);
+  link.addEventListener("click", handlekink);
 });
-console.log(linksInternos);
+
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
+
+const todosElementos = document.querySelectorAll("body *");
+
+function handleElemento(event) {
+  console.log(event.currentTarget);
+}
+
+todosElementos.forEach((elemento) => {
+  elemento.addEventListener("click", handleElemento);
+});
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site.
+function handleClickT(event) {
+  console.log(event.key);
+  if (event.key === "t") {
+    document.documentElement.classList.toggle("textomaior");
+  }
+}
+
+window.addEventListener("keydown", handleClickT);
