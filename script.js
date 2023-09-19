6ac8ca4a59ee85fbc3d915ffac24779471ae7472
Luiz Eduardo <luizleitefe@gmail.com>
2023-09-19 18:26:36 -0300

    sadas
    asdsd

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-// Quando o usuário clicar nos links internos do site,
-// adicione a classe ativo ao item clicado e remova dos
-// demais itens caso eles possuam a mesma. Previna
-// o comportamento padrão desses links
-const linksInternos = document.querySelectorAll('a[href^="#"]');
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector(".menu");
+const copy = document.querySelector(".copy");
 
-function handlekink(event) {
-  event.preventDefault();
-  linksInternos.forEach((link) => {
-    link.classList.remove("ativo");
-  });
-  event.currentTarget.classList.add("ativo");
-}
+const cloneMenu = menu.cloneNode(true);
 
-linksInternos.forEach((link) => {
-  link.addEventListener("click", handlekink);
-});
+copy.appendChild(cloneMenu);
+// Selecione o primeiro DT da dl de Faq
+const faqLista = document.querySelector(".faq");
+const primeiroDT = document.querySelector(".dt");
 
-// Selecione todos os elementos do site começando a partir do body,
-// ao clique mostre exatamente quais elementos estão sendo clicados
+// Selecione o DD referente ao primeiro DT
+const primeiroDD = primeiroDT.nextElementSibling;
 
-const todosElementos = document.querySelectorAll("body *");
-
-function handleElemento(event) {
-  console.log(event.currentTarget);
-}
-
-todosElementos.forEach((elemento) => {
-  elemento.addEventListener("click", handleElemento);
-});
-
-// Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove() remove um elemento
-
-// Se o usuário clicar na tecla (t), aumente todo o texto do site.
-function handleClickT(event) {
-  console.log(event.key);
-  if (event.key === "t") {
-    document.documentElement.classList.toggle("textomaior");
-  }
-}
-
-window.addEventListener("keydown", handleClickT);
+// Substitua o conteúdo html de .faq pelo de .animais
