788f93461cedfea1fb573a51148354b6401abed3
Luiz Eduardo <luizleitefe@gmail.com>
2024-01-18 16:31:14 -0300

    voltando

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-// Qual o resultado da seguinte expressão?
-var total = 10 + (5 * 2) / 2 + 20;
+// Crie uma função para verificar se um valor é Truthy
+function verificar(valor) {
+  return !!valor;
+}
+verificar();
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetro(lado) {
+  console.log(2 * lado + 2 * lado);
+}
+perimetro(5);
 
-// Crie duas expressões que retornem NaN
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  console.log(`${nome} ${sobrenome}`);
+}
+nomeCompleto("andre", "cachorro");
+// Crie uma função que verifica se um número é par
+function naoSei(numero) {
+  var modulo = numero % 2;
+  if (modulo === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+naoSei();
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
 
-// Somar a string '200' com o número 50 e retornar 250
-var string = +"200";
-var numer1 = 50;
-soma = string + numer1;
+function verific(item) {
+  console.log(typeof item);
+}
+verific("ola");
 
-// Incremente o número 5 e retorne o seu valor incrementado
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+addEventListener("scroll", function () {
+  console.log("luiz");
+});
 
-// Como dividir o peso por 2?
-var numero = +"80" / 2;
-var unidade = "kg";
-var peso = numero + unidade; // '80kg'
-console.log(peso);
+// Corrija o erro abaixo
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  var totalPaises = 193;
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(80);
